8e8db996d111c76b4605a1691a9cad695622ab15
EvgeniyTLT <Toy444@ya.ru>
2022-08-07 00:44:50 +0300

    add

--- app/src/main/java/com/example/battomnavigationviewlesson/MainActivity.kt
+++ app/src/main/java/com/example/battomnavigationviewlesson/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.battomnavigationviewlesson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.battomnavigationviewlesson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bNav.selectedItemId = R.id.item3
+        binding.bNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.item1 -> {
+                    Toast.makeText(this, "Item1", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item2 -> {
+                    Toast.makeText(this, "Item2", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item3 -> {
+                    Toast.makeText(this, "Item3", Toast.LENGTH_SHORT).show()
+                }
+                R.id.item4 -> {
+                    Toast.makeText(this, "Item4", Toast.LENGTH_SHORT).show()
+                }
+            }
+            true
+        }
     }
 }
